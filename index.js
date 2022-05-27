2b1b67a3c454523a8e35f743d01fadefba1f2a06
Marco Berna <mbernaildeons@gmail.com>
2022-05-27 02:04:44 -0500

    nav-desktop: se ha agregado la barra de navgeacion para desktop

--- index.js
+++ index.js
@@ -14,3 +14,25 @@ menubtn.addEventListener("click",()=>{
         menubtn.style.display = "none"
    
 })
+// BARRA DE MENU PARA DESKTOP
+const closeDesk= document.getElementById("close-item-desk");
+const contenedorDesk = document.getElementById("contenedor-header-nav-desktop")
+const linkDesk = document.getElementById("producto-link");
+linkDesk.addEventListener("click",()=>{
+    if (contenedorDesk.clientWidth > 0) {
+        
+        contenedorDesk.style.display = "none"
+    }else{
+        contenedorDesk.style.display = "flex"
+
+    }
+    // menubtn.style.display = "block"
+
+})
+
+closeDesk.addEventListener("click",()=>{
+    // e.preventDefault()
+        contenedorDesk.style.display = "none" 
+        // menubtn.style.display = "none"
+   
+})
